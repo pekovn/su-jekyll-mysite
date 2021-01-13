b130d7acbe941bd9f8d827acc1b45cff5439c927
Pavel <pvl.at.ivanov@gmail.com>
2021-01-13 20:38:43 +0200

    re test

--- docs/scripts/site.js
+++ docs/scripts/site.js
@@ -1,3 +1,7 @@
 window.addEventListener('DOMContentLoaded', e => {
-    document.getElementsByTagName("body")[0].classList.add('loaded');
+
+    setTimeout(function () {
+
+        document.getElementsByTagName("body")[0].classList.add('loaded');
+    }, 100);
 })
